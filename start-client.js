a67434fbc599539cc1df38ce61dc1695f6ad5298
Ahmad Ferdous Bin Alam <ahmadferdous@gmail.com>
2017-11-04 18:58:12 -0400

    Add project skeleton with Express and React in mind.
    Didn't want to use any advanced starter kit. Instead, I followed
    the starter kits described in:
    a) Create React App with an Express Backend:
    https://daveceddia.com/create-react-app-express-backend/
    b) How to get "create-react-app" to work with your API:
    https://www.fullstackreact.com/articles/using-create-react-app-with-a-server/

---
+++ start-client.js
@@ -0,0 +1,6 @@
+// Copied from
+// https://github.com/fullstackreact/food-lookup-demo/blob/master/start-client.js
+// See: https://www.fullstackreact.com/articles/using-create-react-app-with-a-server/
+const args = [ 'start' ];
+const opts = { stdio: 'inherit', cwd: 'client', shell: true };
+require('child_process').spawn('npm', args, opts);
