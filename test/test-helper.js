792c5d56343ad7f4230a2214c9826ecdb6293766
Ahmad Ferdous Bin Alam <ahmadferdous@gmail.com>
2017-11-06 07:21:45 -0500

    Drop test database before starting test. Clean up afterwards.

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,9 +1,12 @@
 import * as db from '../db';
+const TEST_DB = 'rouletteTest';
 
 before(async () => {
-  await db.connectToMongoDb();
+  const dbConnection = await db.connectToMongoDb();
+  await dbConnection.dropDatabase(TEST_DB);
 });
 
 after(async () => {
+  await db.getMongoDbConnection().dropDatabase(TEST_DB);
   await db.getMongoDbConnection().close(true);
 });
